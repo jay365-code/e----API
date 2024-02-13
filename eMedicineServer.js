b50be67c2d1dd3062339a8c5d1eca48b8a8623f7
jay365 <jhlee@gubed.co.kr>
2024-02-13 17:42:57 +0900

    change port to 80

--- eMedicineServer.js
+++ eMedicineServer.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 // Middleware to parse JSON requests
 app.use(express.json());
